fe7d1f2577ca769acb1f90b08787004030dd8687
Toluwase Shoniran <140632377+Extraterrestrial-T@users.noreply.github.com>
2026-04-16 17:50:49 +0100

    sligth fic to the logging for read only deployment environments

--- app/main.py
+++ app/main.py
@@ -18,8 +18,8 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler(LOG_PATH),
-        logging.StreamHandler(sys.stderr)
+        #logging.FileHandler(LOG_PATH),
+        logging.StreamHandler(sys.stdout)
     ]
 )
 logger = logging.getLogger("trafficly")
